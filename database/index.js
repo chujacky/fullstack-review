d42fa2301240539f1512f1755c88280fba22ad5c
chujacky <jacky9407@gmail.com>
2018-12-14 13:22:29 -0800

    Added Schmea

--- database/index.js
+++ database/index.js
@@ -2,15 +2,24 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
-  // TODO: your schema here!
+  repo_id: Number,
+  user: String,
+  description: String,
+  repo: String,
+  followers: 0
+
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
 
-let save = (/* TODO */) => {
+let save = (err, Repo) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  if (err) {
+    return console.error(err);
+  }
+  console.log('created schema!')
 }
 
 module.exports.save = save;
